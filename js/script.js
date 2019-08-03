9837f0182866599b6a3c391eecf764025fd100eb
luanfonsecap <luanfonsecap@icloud.com>
2019-08-03 14:23:40 -0300

    button e script menu responsivo

---
+++ js/script.js
@@ -0,0 +1,7 @@
+document.querySelector('.abrir-menu').onclick = function () {
+    document.documentElement.classList.add('menu-ativo');
+}
+
+document.querySelector('.fechar-menu').onclick = function () {
+    document.documentElement.classList.remove('menu-aitvo');
+}
