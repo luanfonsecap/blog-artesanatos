76fb0852857208a2dd49b8011114fbfa9c4345d8
luanfonsecap <luanfonsecap@icloud.com>
2020-03-30 10:35:26 -0300

    desfazendo alterações e  movendo arquivos para diretorio src

--- build/js/script.js
+++
@@ -1,7 +0,0 @@
-document.querySelector('.abrir-menu').onclick = function () {
-    document.querySelector('.nav').classList.add('menu-ativo');
-}
-
-document.querySelector('.fechar-menu').onclick = function () {
-    document.querySelector('.nav').classList.remove('menu-ativo');
-}
